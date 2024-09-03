023937f14895d9532d3f531faa2dd94ac0dace06
Mrhouse <127476912+mrhouse-sweet@users.noreply.github.com>
2024-09-03 10:18:43 +0800

    Update gcc.sh

--- code/src/gcc.sh
+++ code/src/gcc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ main.cpp  -L./include -lmylibti5 -L./include/can -lmylibscan -lcontrolcan -lspdlog -lfmt -ludev -o move_sov
+g++ main.cpp  -L./include -lmylibti5 -L./include/can -I/usr/include/python3.10 -lpython3.10 -lmylibscan -lcontrolcan -lspdlog -lfmt -ludev -o move_sov
